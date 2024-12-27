29013d9f9c8d454981b73df1bee77516a89ad45e
musaab-khan <musaabkhan4511@gmail.com>
2024-12-27 23:34:45 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-// import React from 'react'
-// import HeroSection from './components/HeroSection'
-// import Features from './components/Features'
-// import HowItWorks from './components/HowItWorks'
-// import Footer from './components/Footer'
-// const Home = () => {
-//   return (
-//     <div>
-//     <HeroSection></HeroSection>
-//     <Features></Features>
-//     <HowItWorks></HowItWorks>
-//     <Footer></Footer>
-//     </div>
-//   )
-// }
-
-// export default Home
-
 import React from 'react'
-import ProjectDetails from './components/ProjectDetails'
-
-const page = () => {
+import HeroSection from './components/HeroSection'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
+const Home = () => {
   return (
     <div>
-      <ProjectDetails></ProjectDetails>
+    <HeroSection></HeroSection>
+    <Features></Features>
+    <HowItWorks></HowItWorks>
+    <Footer></Footer>
     </div>
   )
 }
 
-export default page
+export default Home
+
+// import React from 'react'
+// import ProjectDetails from './components/ProjectDetails'
+
+// const page = () => {
+//   return (
+//     <div>
+//       <ProjectDetails></ProjectDetails>
+//     </div>
+//   )
+// }
+
+// export default page
