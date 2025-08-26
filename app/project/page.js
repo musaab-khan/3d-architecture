41da420cb5155476b787a44bb4b9ba863cf6a5e1
musaab-khan <musaabkhan4511@gmail.com>
2025-08-26 14:30:00 +0500

    Update and rename page.txt to page.js

---
+++ app/project/page.js
@@ -0,0 +1,100 @@
+import User from "../components/user";
+import {cache} from 'react'
+
+const fetchVideoData = cache(async (video_id) => {
+  let videoData = {
+    id: null,
+    username: "",
+    tags: [],
+    music: "",
+    videoUrl: "",
+    imageUrl: "",
+    title: "",
+    description: "",
+  };
+  
+  try {
+    const response = await fetch(`https://api.carets.tv/api/v1/videos/singleVideo/${video_id}`, {
+      cache: "no-store",
+    })
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const { data: video } = await response.json();
+
+    videoData = {
+      id: video.id || null,
+      username: video.users?.[0]?.username || "",
+      tags: video.video_description ? video.video_description.split(" ") : [],
+      music: video.sounds?.[0]?.name || "",
+      videoUrl: video.video_url || "",
+      imageUrl: video.image_url || "",
+      title: video.video_title || "",
+      description: video.video_description || "",
+    };
+  } catch (error) {
+    console.error("Error fetching video:", error);
+  }
+
+  return videoData;
+});
+
+export async function generateMetadata({ searchParams }) {
+    const {video_id} = await searchParams;
+  
+    const videoData = await fetchVideoData(video_id);
+
+    return {
+        title: "Video Page",
+        openGraph: {
+            title: videoData.title,
+            type: "video.other",
+            description: videoData.description,
+            url: `https://carets.tv/videos?video_id=${videoData.id}`,
+            images: [
+                {
+                type: "image/gif",
+                url: videoData.imageUrl,
+                secureUrl: videoData.imageUrl,
+                width: 400,
+                height: 300,
+                },
+            ],
+            videos: [
+                {
+                type: "video/mp4",
+                url: videoData.videoUrl,
+                secureUrl: videoData.videoUrl,
+                width: 1200,
+                height: 630,
+                },
+            ],
+        },
+        twitter: {
+            card: "player",
+            title: videoData.title,
+            description: videoData.description,
+            image: videoData.imageUrl,
+            player: {
+                url: `https://carets.tv/videos?video_id=${videoData.id}`,
+                width: 1200,
+                height: 630,
+            },
+        },
+    };
+}
+
+export default async function VideoPage({ searchParams }) {
+    const {video_id} = await searchParams;
+    const videoData = await fetchVideoData(video_id);
+
+    return (
+      <>
+        <div className="customContainer">
+            <div className="mt-14">
+                <User data={videoData} />
+            </div>
+        </div>
+      </>
+    );
+}
